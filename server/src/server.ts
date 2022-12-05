007bfed7b6bfd358d6cd5a2a7536dd196ec53e62
Thiago Peres <thiago.mourao.peres@gmail.com>
2022-12-05 01:37:03 -0300

    Geração e Validação de JWT e Criação da Rota de Perfil (/me)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 
-import { poolRoutes } from './routes/pool'
-import { guessRoutes } from './routes/guess'
-import { userRoutes } from './routes/user'
+import { poolRoutes } from "./routes/pool"
+import { authRoutes } from "./routes/auth"
+import { gameRoutes } from "./routes/game"
+import { guessRoutes } from "./routes/guess"
+import { userRoutes } from "./routes/user"
 
 async function bootstrap() {
   const fastify = Fastify({
@@ -14,7 +17,13 @@ async function bootstrap() {
     origin: true,
   })
 
+  await fastify.register(jwt, {
+    secret: 'NLWCOPA',
+  })
+
   await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(gameRoutes)
   await fastify.register(guessRoutes)
   await fastify.register(userRoutes)
 
