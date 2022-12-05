5fe303441b1b9b6f3a462776af599678441b8725
Thiago Peres <thiago.mourao.peres@gmail.com>
2022-12-05 01:00:29 -0300

    Criação de Usuário (Acess Token Google)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,7 +6,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
-import { Pools } from "./src/screens/Pools";
+import { SignIn } from "./src/screens/Signin";
 import { Loading } from "./src/components/Loading";
 
 import { THEME } from "./src/styles/theme";
@@ -28,7 +28,7 @@ export default function App() {
           translucent
         />
 
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <SignIn /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
