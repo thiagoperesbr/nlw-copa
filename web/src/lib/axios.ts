d6455df06ee7b41204a8bf5479bbe6aede70d33e
Thiago Peres <thiago.mourao.peres@gmail.com>
2022-12-21 18:21:31 -0300

    Ajustes no Axios para Deploy Railway

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export const api = axios.create({
   //baseURL: 'http://192.168.0.231:3333/'
-  baseURL: 'https://nlw-copa-6d80.onrender.com'
+  baseURL: 'https://nlw-copa-server.up.railway.app/'
 })
