43712832f81ce3576af8a80ec29ce2e4d258d196
Thiago Peres <thiago.mourao.peres@gmail.com>
2022-12-21 04:11:00 -0300

    Ajustes no Axios para Deploy

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.0.231:3333/'
+  //baseURL: 'http://192.168.0.231:3333/'
+  baseURL: 'https://nlw-copa-6d80.onrender.com'
 })
